790db43965dbd42ab2f1643d09ef7768665100c1
Rizhi_KotE <d1mqn1ch@mail.ru>
2017-05-11 22:41:50 +0300

    main

---
+++ src/main/java/StartServer.java
@@ -0,0 +1,14 @@
+import org.springframework.context.support.GenericGroovyApplicationContext;
+import server.soap.HandbookSoapPublisher;
+import server.thrift.ThriftServer;
+
+public class StartServer {
+    public static void main(String[] args) throws Exception {
+        GenericGroovyApplicationContext context = new GenericGroovyApplicationContext("serverContext.groovy");
+//        HandbookSoapPublisher soap = context.getBean(HandbookSoapPublisher.class);
+//        soap.publish();
+        ThriftServer thrift = context.getBean(ThriftServer.class);
+        thrift.run();
+    }
+}
+
